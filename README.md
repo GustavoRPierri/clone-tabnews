cae10788cddc9aad3ab749532c231f32387fcc3f
GustavoRPierri <gustavoromanopierri@hotmail.com>
2026-01-21 22:03:52 -0300

    adiciona scripts lint:check e lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do https://www.tabnews.com.br no curso https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 function Home() {
-  return <h1>Mozãozinho, eu te amo S2. Se você me ama, dá uma risadinha!
-  </h1>;
+  return <h1>Mozãozinho, eu te amo S2. Se você me ama, dá uma risadinha!</h1>;
+}
+
+function teste() {
+  console.log("Teste");
+}
+
+function teste2() {
+  console.log("Teste 2");
 }
 
 export default Home;
